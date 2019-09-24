f50ee0433b065743a56111adc0ba27304c1d09ea
Artur <artur.borbela1122@gmail.com>
2019-09-24 15:15:07 +0300

    01a done

--- assignment01a.py
+++ assignment01a.py
@@ -7,83 +7,18 @@ For those who are already familiar with Python – write the best code you can t
 
 """
 
-poem = '''
-This is the house that Jack built.
 
-This is the malt 
-That lay in the house that Jack built.
 
-This is the rat, 
-That ate the malt 
-That lay in the house that Jack built.
 
-This is the cat, 
-That kill'd the rat, 
-That ate the malt 
-That lay in the house that Jack built.
+list1 = ["This is ","That "]
+list2 = ["lay in ", "ate ","killed ","worried ", "tossed ", "milk'd ", "kisse'd ",
+                                                       "married ","waked ","kept "]
+list3 = ["the house that Jack built. "  ,  "the malt "  ,  "the rat, "  ,  "the cat,","the dog, ",
+         "the cow with the crumpled horn,","the maiden all forlorn,","the man all tatter'd and torn, ",
+         "the priest all shaven and shorn,","the cock that crow'd in the morn, ","the farmer sowing his corn, " ]
+for i in range(len(list3)) :
+    print(list1[0] + list3[i])
+    for j in range(i-1,-1 ,-1) :
+        print(list1[1] + list2[j] + list3[j])
+    print('\n\n\n')
 
-This is the dog, 
-That worried the cat, 
-That kill'd the rat, 
-That ate the malt 
-That lay in the house that Jack built.
-
-This is the cow with the crumpled horn, 
-That toss'd the dog, 
-That worried the cat, 
-That kill'd the rat, 
-That ate the malt 
-That lay in the house that Jack built.
-
-This is the maiden all forlorn, 
-That milk'd the cow with the crumpled horn, 
-That tossed the dog, 
-That worried the cat, 
-That kill'd the rat, 
-That ate the malt 
-That lay in the house that Jack built.
-
-This is the man all tatter'd and torn, 
-That kissed the maiden all forlorn, 
-That milk'd the cow with the crumpled horn,
-That tossed the dog, 
-That worried the cat, 
-That kill'd the rat, 
-That ate the malt 
-That lay in the house that Jack built.
-
-This is the priest all shaven and shorn, 
-That married the man all tatter'd and torn, 
-That kissed the maiden all forlorn, 
-That milked the cow with the crumpled horn,
-That tossed the dog, 
-That worried the cat, 
-That kill'd the rat, 
-That ate the malt 
-That lay in the house that Jack built.
-
-This is the cock that crow'd in the morn, 
-That waked the priest all shaven and shorn, 
-That married the man all tatter'd and torn, 
-That kissed the maiden all forlorn, 
-That milk'd the cow with the crumpled horn, 
-That tossed the dog, 
-That worried the cat, 
-That kill'd the rat, 
-That ate the malt 
-That lay in the house that Jack built.
-
-This is the farmer sowing his corn, 
-That kept the cock that crow'd in the morn, 
-That waked the priest all shaven and shorn,
-That married the man all tatter'd and torn, 
-That kissed the maiden all forlorn, 
-That milk'd the cow with the crumpled horn,
-That tossed the dog, 
-That worried the cat, 
-That killed the rat, 
-That ate the malt 
-That lay in the house that Jack built.
-'''
-
-print(poem)
